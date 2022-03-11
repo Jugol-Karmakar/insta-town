5c7373e4cdaaf1e8365d23a88d57b95b96db1da0
JugolKarmakar <jugol.cse@gmail.com>
2022-03-11 21:47:06 +0600

    finish commit

--- js/app.js
+++ js/app.js
@@ -30,7 +30,7 @@ const reportPost = (id) => {
 
 const displayContent = (text) => {
   return text.length < 30
-    ? " Lorem, ipsum dolor sit amet co<span class='fw-bold'>... read more</span>"
+    ? text
     : text.slice(0, 30) + "<span class='fw-bold'>... read more</span>";
 };
 
@@ -128,9 +128,9 @@ const createPost = (post) => {
                   <div class="post__description">
                     <small>
                       <a class="post__name--underline" href="#">
-                          ${post.comments?.user}
+                        ${post?.comments[0]?.user}
                       </a>
-                      ${post.comments?.text}
+                       ${post?.comments[0]?.text}
                     </small>
                   </div>
                   <span class="post__date-time">30 minutes ago</span>
